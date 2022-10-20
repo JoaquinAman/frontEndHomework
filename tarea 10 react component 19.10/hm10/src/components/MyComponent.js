49fd88ac5a02c3581f65de0f7171699a67e28a28
joaquin <joaco.aman@gmail.com>
2022-10-20 10:05:46 -0300

    adding hm 10 react component

---
+++ tarea 10 react component 19.10/hm10/src/components/MyComponent.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import './MyComponent.css'
+
+const firstCar = {
+    make: 'Ford',
+    year: 1969
+  };
+const secondCar = {
+    make: 'Audi',
+    year: 2007
+  };
+
+function MyComponent(){
+
+   const [car, setCar] = useState();
+
+    useEffect(() => {
+        setTimeout(function () {
+                    setCar([firstCar, secondCar])
+            }, 3000);
+        },[car])
+        console.log(car)
+    return (
+        <div className='container'>
+            { !car ?
+                <div className='loader'> Loading </div>
+                    : 
+                (   <>
+                        <div className='card1'>
+                            <h1>{ car[0].make} - {car[0].year}</h1>
+                        </div>
+                        <div className='card2'>
+                            <h2>{car[1].make} - {car[1].year}</h2>    
+                        </div>
+                    </>
+                ) 
+            }
+        </div>
+    )
+}
+export default MyComponent
